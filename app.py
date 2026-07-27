4461b5d3d2d505f3c2caeeddaad05c2f1f1bdc55
Babli <bablipandit8178@gmail.com>
2026-07-27 17:34:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,16 +107,7 @@ always use different color or styling"""
 final_prompt = prompt + resume_maker_prompt()
 
 user_details = """user details: given below:
-Give Python developer Resume
-name Babli
-i am a AI engineer
-courses Agentic AI and Gen AI
-phone no 7678232349
-email id bablipandit8178@gamil.com
-Technical skill python c c++ javascript CS HTML DSA Ml
-education passed out james convent school
-currently persuing BCA from Institute of Innovation in Technology
-Languages Hindi English"""
+Give Python Developer Resume"""
 
 query = final_prompt + user_details
 
