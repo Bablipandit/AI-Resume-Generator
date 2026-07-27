77dddc763c63bcdb1a2e189d3117fa66de71c3db
Babli <bablipandit8178@gmail.com>
2026-07-27 13:40:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 # streamlit: Web based app making
-# lite python framewrok
+# lite python framework
 
 st.title("AI Resume Maker")
 
 st.markdown("""## User can create or
-download AI created Resume based onn high ATS
+download AI created Resume based on high ATS
 Score""")
 
 
-#==========AGENT CODE==========
-# Step 2:
+#==================AGENT CODE===================
+# Step 2: Load Modules
 
 import os
 import time
@@ -26,26 +26,26 @@ import streamlit as st
 from langchain_community.document_loaders import PyMuPDFLoader
 
 
-# ==============API KEY LOAD===============
+# ================API KEY LOAD===================
 
 GOOGLE_API_KEY = st.sidebar.text_input("GOOGLE_API_KEY",type="password")
 GROQ_API_KEY = st.sidebar.text_input("GROQ_API_KEY",type="password")
 TAVILY_API_KEY = st.sidebar.text_input("TAVILY_API_KEY",type="password")
 
 
-# =============MODEL BUILDING==============
+# ===============MODEL BUILDING=============
 model = ChatGoogleGenerativeAI(
     model = 'gemini-3.5-flash-lite',
     google_api_key = GOOGLE_API_KEY
 )
+
 # tool
 def search_recent_news_jobs(query):
   """This function helps to search
-  recent news or recents jobs
+  recent news or recent jobs
   related to given search query
   suppose user write Python Developer jobs
   It should return trending news and jobs link"""
-
   client = TavilyClient(
       api_key = TAVILY_API_KEY
       )
@@ -55,18 +55,18 @@ def search_recent_news_jobs(query):
 
 # agent creation
 from langchain.agents import create_agent
+
 agent = create_agent(
     model = model,
     tools = [search_recent_news_jobs]
 )
 
 
-
-# ==========PROMPT GENERATOR=============
-def prompt_generator(agent):
+# ==== PROMPT GENERATOR================
+def prompt_generator(agent = agent):
   """This function help to give detailed prompt
-  followed by chain of thoughts and persona
-  based prompting, main task is to give
+  followed by Chain of thoughts and
+  persona based prompting, main task is to give
   detailed prompt to build Resume for
   Students or Experienced person
   Based on their given personal information.
@@ -77,17 +77,17 @@ def prompt_generator(agent):
   detailed prompt to build Resume for
   Students or Experienced person
   Based on their given personal information.
-  System Instructer I want Model to generate resume
-  in HTML format include that in prompt"""
+  System Instruction I want Model to generate resume
+  in HTML format , include that in prompt"""
 
   response = agent.invoke(prompt)
   file_name = 'prompt.py'
   with open(file_name, 'w') as f:
     f.write(response.content[-1]['text'])
-  return "Prompt file generated Succesfully, agent can read it"
+  return "Prompt file generated Successfully, agent can read it"
 
 prompt_generator(model)
-# Tool 2:
+# tool 2:
 def resume_maker_prompt():
   """This function just gives
   updated prompt for model"""
@@ -97,34 +97,31 @@ def resume_maker_prompt():
   return prompt
 
 resume_maker_prompt()
-# ===========GENERATE RESUME=============
+# ===========GENERATE RESUME========
 prompt = """You are a helpful AI assistant
 with job resume maker, your task is to give
 HTML format resume, with proper designing using recent CSS and JS
 code, with professional design Format.
-user will upload data and return HTML format resume
+User will upload data and return HTML format resume
 always use different color or styling"""
 
 final_prompt = prompt + resume_maker_prompt()
 
 user_details = """user details: given below:
-Give Python Developer Resume
-name Babli
-courses Agentic AI and Gen AI
-phone no 7678232349
-email id bablipandit8178@gamil.com
-Technical skill python c c++ javascript CSS HTML DSA
-education passed out james convent school
-currently persuing BCA from Institute of Innovation in Technology
-Languages Hindi English"""
+Give Python Developer Resume"""
 
 query = final_prompt + user_details
 
 if st.button("Generate Resume"):
   with st.spinner("Running Agent...."):
+
     response = agent.invoke({'messages':[{'role':'user','content':query}]})
     code = response['messages'][-1].content[-1]['text']
 
     #st.markdown(code)
     st.html(code, width="stretch", unsafe_allow_javascript=True)
-      
+
+
+
+
+
